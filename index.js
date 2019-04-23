bfb1d91afff01c233686f3fb2834afaee0d3d078
Mera-Stackhouse <mera.stackhouse@gmail.com>
2019-04-22 17:14:30 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+main = document.getElementById('main')
+main.remove()
+
+const newHeader = document.createElement('h1')
+
+document.body.appendChild(newHeader)
+
+newHeader.id = 'victory'
+
+newHeader.textContent = "MERA is the champion"
